22da76d753b95f2edf5410b22da4ac09e9ee368b
Shafer Oudeh <shafer.oudeh@gmail.com>
2020-11-16 18:37:57 -0500

    added api and unit tests

--- src/js/apiHelper.js
+++ src/js/apiHelper.js
@@ -1,4 +1,4 @@
-
+const fs= require('fs');
 
 
 
@@ -8,8 +8,19 @@ module.exports = class ApiHelper {
   static async getResponseForApiRequest(apiEndpointName){
 
       ///write custom code here
+      /* maybe like looping through magic card files,
+      filtering, stor[filepathgoeshere]ing into variable, etc
+      */
 
-      return {success:true, apiEndpointName: apiEndpointName, outputData: {}}
+      let jsondata = fs.readFileSync(__dirname+
+      "/../assets/buttdata/butt.json");
+
+      let data = JSON.parse(jsondata);
+
+      return {success:true,
+         apiEndpointName: apiEndpointName,
+         outputData: data,
+       outputJson: jsondata.toString()}
   }
 
 
--- test/mochatest.js
+++ test/mochatest.js
@@ -1,8 +1,18 @@
 var assert = require('assert');
+const ApiHelper = require('../src/js/apiHelper')
+
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
+
+    it('should return API results', async()=> {
+      let results = await ApiHelper.getResponseForApiRequest("hello");
+      console.log("results are" , results);
+      assert.ok(results);
+    });
+
   });
 });
